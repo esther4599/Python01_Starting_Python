cbf3c2b7239cf4b707b9b10ef81e9b0425cc7479
esther4599 <33406188+esther4599@users.noreply.github.com>
2020-05-25 23:51:43 +0900

    Add files via upload

---
+++ 10.딕셔너리와튜플/01.dic_basic.py
@@ -0,0 +1,31 @@
+#wintable = 승리조건을 조장해둔 dictionary
+wintable = {
+    '가위' : '보',
+    '바위' : '가위',
+    '보' : '바위'
+}
+#HashMap과 유사. 앞에 적힌 부분은 key, 뒤에는 value
+#print(wintable['가위']) #검색은 key 값으로
+
+words = ['a','b','c']
+#print(words[1]) #list는 검색을 index num으로 한다.
+
+def rsp(mine, yours) :
+    if mine == yours:
+        return 'draw'
+    elif wintable[mine] == yours :
+        return 'win'
+    else :
+        return 'lose'
+result = rsp('가위', '바위')
+
+masseges = {
+    'win' : '승리',
+    'lose' : '패배',
+    'draw' : '비겼다'
+}
+
+print(masseges[result])
+
+
+
